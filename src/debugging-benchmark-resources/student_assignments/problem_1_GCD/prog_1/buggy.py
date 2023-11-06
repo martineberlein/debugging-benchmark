e0f0529a30f5ab371257d713303258109a030c9f
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-11-06 11:02:56 +0100

    added student assignments

---
+++ src/debugging-benchmark-resources/student_assignments/problem_1_GCD/prog_1/buggy.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    def gcd(self, A, B):
+        # code here
+        while (A%B!=0):
+            A,B=B,A%B
+            return B
+
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        A,B = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.gcd(A,B))
+# } Driver Code Ends
