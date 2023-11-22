e02156924965856a9f36ed1edb0b09ac2a0a54b3
Kai <kai.werk@gmx.de>
2023-11-22 10:37:48 +0100

    refactoring calculator to better fit design concept

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,21 +2,30 @@ import unittest
 
 from fuzzingbook.Parser import EarleyParser, tree_to_string, is_valid_grammar
 from debugging_framework.oracle import OracleResult
-from debugging_benchmark.calculator.calculator import grammar, initial_inputs, oracle
+from debugging_benchmark.calculator.calculator import CalculatorBenchmarkRepository, CalculatorBenchmarkProgram
 
 
 class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.repo = CalculatorBenchmarkRepository()
+        self.program = self.repo.build()
+        
+    def test_build(self):
+        self.assertIsInstance(self.program, CalculatorBenchmarkProgram)
+
     def test_calculator_grammar(self):
-        self.assertTrue(is_valid_grammar(grammar))
+        self.assertTrue(is_valid_grammar(self.program.get_grammar()))
 
     def test_calculator_initial_inputs(self):
-        parser = EarleyParser(grammar)
+        parser = EarleyParser(self.program.get_grammar())
+        inputs = self.program.get_initial_inputs()
 
-        for inp in initial_inputs:
+        for inp in inputs:
             for tree in parser.parse(inp):
                 self.assertEqual(inp, tree_to_string(tree))
 
     def test_calculator_oracle(self):
+        oracle = self.program.get_oracle()
         inputs = [
             ("sqrt(-900)", OracleResult.FAILING),
             ("cos(10)", OracleResult.PASSING),
