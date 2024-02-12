c7f98bd5083f4c691b42c0484add6cb1dd648995
Martin Eberlein <schuldner-01.vordach@icloud.com>
2024-02-12 11:40:53 +0100

    added separate resource folder for coverage tests

--- test/test_coverage.py
+++ test/test_coverage.py
@@ -1,17 +1,7 @@
 import unittest
 
 from debugging_framework.coverage import population_coverage, population_branch_coverage
-
-
-# Program should not be moved, as this affects the executed lines of codes;
-# moving might resolve in failing tests
-def program_simple(inp: str) -> int:
-    if inp.startswith("sqrt"):
-        return 1
-    elif inp.startswith("cos"):
-        return 2
-    elif inp.startswith("tan"):
-        return 3
+from .resources.program_simple_coverage import program_simple
 
 
 class TestCoverage(unittest.TestCase):
@@ -20,7 +10,7 @@ class TestCoverage(unittest.TestCase):
 
         self.assertEqual(cumulative, [2])
         # lines of the program should not change!
-        self.assertEqual(all_coverage, {("program_simple", 9), ("program_simple", 10)})
+        self.assertEqual(all_coverage, {("program_simple", 2), ("program_simple", 3)})
 
     def test_coverage_population(self):
         all_coverage, cumulative = population_coverage(
@@ -32,14 +22,14 @@ class TestCoverage(unittest.TestCase):
         self.assertEqual(
             all_coverage,
             {
-                ("program_simple", 9),
-                ("program_simple", 10),
-                ("program_simple", 11),
-                ("program_simple", 13),
-                ("program_simple", 14),
+                ("program_simple", 2),
+                ("program_simple", 3),
+                ("program_simple", 4),
+                ("program_simple", 6),
+                ("program_simple", 7),
             },
         )
-        self.assertNotIn(("program_simple", 12), all_coverage)
+        self.assertNotIn(("program_simple", 5), all_coverage)
 
     def test_branch_coverage(self):
         all_coverage, cumulative = population_branch_coverage(
@@ -51,10 +41,10 @@ class TestCoverage(unittest.TestCase):
         self.assertEqual(
             all_coverage,
             {
-                (("program_simple", 9), ("program_simple", 10)),
+                (("program_simple", 2), ("program_simple", 3)),
             },
         )
-        self.assertNotIn((("program_simple", 9), ("program_simple", 11)), all_coverage)
+        self.assertNotIn((("program_simple", 2), ("program_simple", 4)), all_coverage)
 
     def test_branch_coverage_population(self):
         all_coverage, cumulative = population_branch_coverage(
@@ -66,13 +56,13 @@ class TestCoverage(unittest.TestCase):
         self.assertEqual(
             all_coverage,
             {
-                (("program_simple", 9), ("program_simple", 10)),
-                (("program_simple", 9), ("program_simple", 11)),
-                (("program_simple", 11), ("program_simple", 13)),
-                (("program_simple", 13), ("program_simple", 14)),
+                (("program_simple", 2), ("program_simple", 3)),
+                (("program_simple", 2), ("program_simple", 4)),
+                (("program_simple", 4), ("program_simple", 6)),
+                (("program_simple", 6), ("program_simple", 7)),
             },
         )
-        self.assertNotIn((("program_simple", 11), ("program_simple", 12)), all_coverage)
+        self.assertNotIn((("program_simple", 4), ("program_simple", 5)), all_coverage)
 
 
 if __name__ == "__main__":
