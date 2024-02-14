ab200debb07a916ba9c16c99c7c74bab0a1e987d
Kai <kai.werk@gmx.de>
2024-02-14 15:50:40 +0100

    started removing fuzzingbook

--- src/debugging_framework/grammar.py
+++ src/debugging_framework/grammar.py
@@ -1,14 +1,17 @@
-from fuzzingbook.Grammars import is_valid_grammar
+import sys
+from typing import Set, Optional, Tuple, Dict, Any
 
-from typing import Set
-from debugging_framework.types import Grammar
+from isla.derivation_tree import DerivationTree
+
+from debugging_framework.types import Grammar, Expansion, START_SYMBOL, RE_NONTERMINAL
+from debugging_framework.fuzzer import exp_probabilities
 
 def is_valid_grammar(grammar: Grammar,
                      start_symbol: str = START_SYMBOL,
                      supported_opts: Set[str] = set()) -> bool:
     """Check if the given `grammar` is valid.
-       `start_symbol`: optional start symbol (default: `<start>`)
-       `supported_opts`: options supported (default: none)"""
+    `start_symbol`: optional start symbol (default: `<start>`)
+    `supported_opts`: options supported (default: none)"""
 
     defined_nonterminals, used_nonterminals = \
         def_used_nonterminals(grammar, start_symbol)
@@ -53,3 +56,107 @@ def is_valid_grammar(grammar: Grammar,
                 file=sys.stderr)
 
     return used_nonterminals == defined_nonterminals and len(unreachable) == 0
+
+def def_used_nonterminals(grammar: Grammar, start_symbol: 
+                          str = START_SYMBOL) -> Tuple[Optional[Set[str]], 
+                                                       Optional[Set[str]]]:
+    """Return a pair (`defined_nonterminals`, `used_nonterminals`) in `grammar`.
+    In case of error, return (`None`, `None`)."""
+
+    defined_nonterminals = set()
+    used_nonterminals = {start_symbol}
+
+    for defined_nonterminal in grammar:
+        defined_nonterminals.add(defined_nonterminal)
+        expansions = grammar[defined_nonterminal]
+        if not isinstance(expansions, list):
+            print(repr(defined_nonterminal) + ": expansion is not a list",
+                  file=sys.stderr)
+            return None, None
+
+        if len(expansions) == 0:
+            print(repr(defined_nonterminal) + ": expansion list empty",
+                  file=sys.stderr)
+            return None, None
+
+        for expansion in expansions:
+            if isinstance(expansion, tuple):
+                expansion = expansion[0]
+            if not isinstance(expansion, str):
+                print(repr(defined_nonterminal) + ": "
+                      + repr(expansion) + ": not a string",
+                      file=sys.stderr)
+                return None, None
+
+            for used_nonterminal in nonterminals(expansion):
+                used_nonterminals.add(used_nonterminal)
+
+    return defined_nonterminals, used_nonterminals
+
+def reachable_nonterminals(grammar: Grammar,
+                           start_symbol: str = START_SYMBOL) -> Set[str]:
+    reachable = set()
+
+    def _find_reachable_nonterminals(grammar, symbol):
+        nonlocal reachable
+        reachable.add(symbol)
+        for expansion in grammar.get(symbol, []):
+            for nonterminal in nonterminals(expansion):
+                if nonterminal not in reachable:
+                    _find_reachable_nonterminals(grammar, nonterminal)
+
+    _find_reachable_nonterminals(grammar, start_symbol)
+    return reachable
+
+def unreachable_nonterminals(grammar: Grammar,
+                             start_symbol=START_SYMBOL) -> Set[str]:
+    return grammar.keys() - reachable_nonterminals(grammar, start_symbol)
+
+def nonterminals(expansion):
+    # In later chapters, we allow expansions to be tuples,
+    # with the expansion being the first element
+    if isinstance(expansion, tuple):
+        expansion = expansion[0]
+
+    return RE_NONTERMINAL.findall(expansion)
+
+def is_nonterminal(s):
+    return RE_NONTERMINAL.match(s)
+
+def opts_used(grammar: Grammar) -> Set[str]:
+    used_opts = set()
+    for symbol in grammar:
+        for expansion in grammar[symbol]:
+            used_opts |= set(exp_opts(expansion).keys())
+    return used_opts
+
+def exp_opts(expansion: Expansion) -> Dict[str, Any]:
+    """Return the options of an expansion.  If options are not defined, return {}"""
+    if isinstance(expansion, str):
+        return {}
+    return expansion[1]
+
+def is_valid_probabilistic_grammar(grammar: Grammar,
+                                   start_symbol: str = START_SYMBOL) -> bool:
+    if not is_valid_grammar(grammar, start_symbol):
+        return False
+
+    for nonterminal in grammar:
+        expansions = grammar[nonterminal]
+        _ = exp_probabilities(expansions, nonterminal)
+
+    return True
+
+def all_terminals(tree: DerivationTree) -> str:
+    (symbol, children) = tree
+    if children is None:
+        # This is a nonterminal symbol not expanded yet
+        return symbol
+
+    if len(children) == 0:
+        # This is a terminal symbol
+        return symbol
+
+    # This is an expanded symbol:
+    # Concatenate all terminal symbols from all children
+    return ''.join([all_terminals(c) for c in children])
--- src/debugging_framework/types.py
+++ src/debugging_framework/types.py
@@ -1,8 +1,14 @@
 from typing import Callable, Union, Sequence, Type, Dict, List, Tuple, Any
 from debugging_framework.input import Input
-
+import re
+#Harness Function for BenchmarkRepositorys
 HARNESS_FUNCTION = Type[Callable[[Union[str, Input]], Sequence[str]]]
 
+#Wieso Option kann man nicht von typing Optional verwenden? mhh eig nicht
 Option = Dict[str, Any]
 Expansion = Union[str, Tuple[str, Option]]
+
+#Grammars
 Grammar = Dict[str, List[Expansion]]
+START_SYMBOL = "<start>"
+RE_NONTERMINAL = re.compile(r'(<[^<> ]*>)')
