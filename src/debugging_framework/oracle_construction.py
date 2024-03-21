e0f27fac20a29bc5ca1d84de740075f753b93873
Martin Eberlein <schuldner-01.vordach@icloud.com>
2024-03-21 15:54:28 +0100

    deep copy in oracle construction

--- src/debugging_framework/oracle_construction.py
+++ src/debugging_framework/oracle_construction.py
@@ -1,27 +1,31 @@
 from typing import Callable, Dict, Type, Optional, Tuple, Any, Sequence
 
+from copy import deepcopy
+
 from debugging_framework.oracle import OracleResult
 from debugging_framework.input import Input
 from debugging_framework.timeout_manager import ManageTimeout
 from debugging_framework.expceptions import UnexpectedResultError
+from debugging_framework.memory_manager import ManageMemory
 
 import contextlib
 
 
-def execute_program(program, param: Sequence, timeout) -> Any:
+def execute_program(program_, param: Sequence, timeout) -> Any:
     with ManageTimeout(timeout):
         # silencing the stdout for PuT
         with contextlib.redirect_stdout(None):
-            return program(*param)
+            dp = deepcopy(param)
+            return program_(*dp)
 
 
 def construct_oracle(
-    program_under_test: Callable,
-    program_oracle: Optional[Callable],
-    error_definitions: Optional[Dict[Type[Exception], OracleResult]] = None,
-    timeout: float = 1,
-    default_oracle_result: OracleResult = OracleResult.UNDEFINED,
-    harness_function: Callable = None,
+        program_under_test: Callable,
+        program_oracle: Optional[Callable],
+        error_definitions: Optional[Dict[Type[Exception], OracleResult]] = None,
+        timeout: float = 1,
+        default_oracle_result: OracleResult = OracleResult.UNDEFINED,
+        harness_function: Callable = None,
 ) -> Callable[[Input], Tuple[OracleResult, Optional[Exception]]]:
     error_definitions = error_definitions or {}
     default_oracle_result = (
@@ -44,12 +48,12 @@ def construct_oracle(
 
 
 def _construct_functional_oracle(
-    program_under_test: Callable,
-    program_oracle: Callable,
-    error_definitions: Dict[Type[Exception], OracleResult],
-    timeout: float,
-    default_oracle_result: OracleResult,
-    harness_function: Callable,
+        program_under_test: Callable,
+        program_oracle: Callable,
+        error_definitions: Dict[Type[Exception], OracleResult],
+        timeout: float,
+        default_oracle_result: OracleResult,
+        harness_function: Callable,
 ):
     def oracle(inp: Input) -> Tuple[OracleResult, Optional[Exception]]:
         # param = list(map(int, str(inp).strip().split()))  # This might become a problem
@@ -59,7 +63,7 @@ def _construct_functional_oracle(
             produced_result = execute_program(program_under_test, param, timeout)
             expected_result = execute_program(program_oracle, param, timeout)
 
-            if expected_result != produced_result:
+            if (expected_result != produced_result) or (type(expected_result) is not type(produced_result)):
                 raise UnexpectedResultError("Results do not match")
         except Exception as e:
             return error_definitions.get(type(e), default_oracle_result), e
@@ -69,11 +73,11 @@ def _construct_functional_oracle(
 
 
 def _construct_failure_oracle(
-    program_under_test: Callable,
-    error_definitions: Dict[Type[Exception], OracleResult],
-    timeout: float,
-    default_oracle_result: OracleResult,
-    harness_function: Callable,
+        program_under_test: Callable,
+        error_definitions: Dict[Type[Exception], OracleResult],
+        timeout: float,
+        default_oracle_result: OracleResult,
+        harness_function: Callable,
 ):
     def oracle(inp: Input) -> Tuple[OracleResult, Optional[Exception]]:
         param = harness_function(str(inp)) if harness_function else str(inp)
